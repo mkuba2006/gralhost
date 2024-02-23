71989b196e63f23169014536c4ea88b9b17ae40a
Jakub Myrta <mkuba2006@gmail.com>
2024-02-23 20:46:20 +0100

    15

--- src/components/static/header/header.jsx
+++ src/components/static/header/header.jsx
@@ -11,7 +11,7 @@ import Fav from '../../fav/cart';
 function Header() {
   const Otx = useContext(OpenContext);
 
-  const changeH = () =>{Otx.setHover(); console.log(Otx.hover);};
+  const changeH = () =>{Otx.setHover(prevHover => !prevHover); console.log(Otx.hover);};
   const changeC = () =>{Otx.setCart(); console.log('cart: ',Otx.seeCart);};
   const changeFavH = () =>{
     Otx.setFavHover();
