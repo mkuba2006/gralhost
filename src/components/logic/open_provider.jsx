7d935ec80bd7f893ec586ac1425832bd85699a40
Jakub Myrta <mkuba2006@gmail.com>
2024-02-23 20:09:32 +0100

    1

--- src/components/logic/open_provider.jsx
+++ src/components/logic/open_provider.jsx
@@ -1,30 +1,32 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useReducer } from 'react';
+import OpenContext from './open_context';
 
-const OpenContext = createContext();
+const reducer = (state, action) => !state;
+const favreducer=(state,action)=>{
+  console.log(action);
+  if(action = "TOGGLE"){
+    return !state;
+  }
+  else if(action = "OPEN"){
+    return true;
+  }
+  else if(action = "CLOSE"){
+    return false;
+  }
+}
+
 
 const OpenProvider = (props) => {
-  const [isOpen, change] = useState(false);
-  const [isHover, changeH] = useState(false);
-  const [isFavhover, changeFavH] = useState(false);
-  const [isCart, changeC] = useState(false);
-  const [isELVisible, changeVisible] = useState(false);
-
-  const toggle = (setter) => () => {
-    setter({ type: "TOGGLE" });
-    console.log("isHover old:", isHover);
-    console.log("isHover new:", !isHover);
-  };
-
-  const close = (setter) => () => {
-    setter({ type: "CLOSE" });
-  };
-
-  const open = (setter) => () => {
-    setter({ type: "OPEN" });
-  };
+  const [isOpen, change] = useReducer(reducer, false);
+  const [isHover, changeH] = useReducer(reducer, false);
+  const [isCart, changeC] = useReducer(reducer, false);
+  const [isELVisible, changeVisible] = useReducer(reducer, false);
+  const [isFavhover, changeFavH] = useReducer(favreducer, false);
 
 
-
+  const toggle = (setter) => () => setter({ type: 'TOGGLE' });
+  const close = (setter) => () => setter({ type: 'CLOSE' });
+  const open = (setter) => () => setter({ type: 'OPEN' });
 
   const OpenCTX = {
     open: isOpen,
@@ -36,6 +38,7 @@ const OpenProvider = (props) => {
     setFavHover: toggle(changeFavH),
     closeFavHover: close(changeFavH),
     openFavHover: open(changeFavH),
+
 
     seeCart: isCart,
     setCart: toggle(changeC),
