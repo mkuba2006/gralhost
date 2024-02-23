239b230c0f5ba1f5af125c9bdc2559fda3f09e18
Jakub Myrta <mkuba2006@gmail.com>
2024-02-23 20:41:44 +0100

    11

--- src/components/logic/open_provider.jsx
+++ src/components/logic/open_provider.jsx
@@ -7,11 +7,11 @@ const reducer = (state, action) => {
 
 const favreducer = (state, action) => {
   console.log(action);
-  if (action === "TOGGLE") {
+  if (action = "TOGGLE") {
     return !state;
-  } else if (action === "OPEN") {
+  } else if (action = "OPEN") {
     return true;
-  } else if (action === "CLOSE") {
+  } else if (action = "CLOSE") {
     return false;
   }
 }
